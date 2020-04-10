621b7ae3fe2d13940ad06d28b2e43b7a21643ebe
pce <info@pc-e.org>
2020-04-10 12:17:28 +0200

    added frontend (temporary build test), added agpl

---
+++ public/precache-manifest.98e0366509b23d76732fabe3b33bb323.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "108a92d7e93fa21523688ac00c31bfb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a535d67083cb81473150",
+    "url": "/static/css/2.e68abe7b.chunk.css"
+  },
+  {
+    "revision": "57347676144e49ff8d99",
+    "url": "/static/css/main.10bb77cd.chunk.css"
+  },
+  {
+    "revision": "a535d67083cb81473150",
+    "url": "/static/js/2.2c8e59fe.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.2c8e59fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57347676144e49ff8d99",
+    "url": "/static/js/main.82307237.chunk.js"
+  },
+  {
+    "revision": "c3b42b4eb4ece7ea66a2",
+    "url": "/static/js/runtime-main.c2d27d72.js"
+  },
+  {
+    "revision": "f14ef0d82eec41630a6abb2f5f1511f4",
+    "url": "/static/media/bg.f14ef0d8.jpg"
+  }
+]);
