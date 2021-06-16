3ace81edef3ef0000d55c05d441e42f233345fda
pce <445226+pce@users.noreply.github.com>
2021-06-16 18:21:50 +0200

    feat: user.create set inital defaults, a mail-hook belongs to the controller
    added commented out
    
    ...

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,26 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  after_initialize :init
+  # before_create :set_role, :if => lambda { |user| user.user_role.nil? }
+
+  # after_create :notify
+  # def notify
+  #   # send notification from controller not model, for example: test or console 
+  #   UserMailer.send_new_user_message(self).deliver
+  # end
+
+  def init
+    # set defaults with ||= except boolean
+    self.user_role = true if self.user_role.nil?
+    self.admin_role = false if self.admin_role.nil?
+    self.supervisor_role = false if self.supervisor_role.nil?
+  end
+
   def generate_new_authentication_token
     token = User.generate_unique_secure_token
     update_attributes authentication_token: token
   end
+
 
 end
